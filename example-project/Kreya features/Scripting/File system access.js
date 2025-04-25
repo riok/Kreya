879bf2058adc06c04eeb83522dfc31497447e7ea
Manuel Allenspach <manuel@riok.ch>
2025-04-25 14:43:08 +0200

    add Script example

---
+++ example-project/Kreya features/Scripting/File system access.js
@@ -0,0 +1,10 @@
+import { readFile } from 'fs/promises';
+import { expect } from 'chai';
+
+const expectedResponse = await readFile('book-1-expected-response.json', 'utf8');
+
+kreyaRest.onCallCompleted(call => {
+  kreya.test('Response should match snapshot', () => {
+    expect(call.response.rawContentText).to.eql(expectedResponse);
+  });
+});
